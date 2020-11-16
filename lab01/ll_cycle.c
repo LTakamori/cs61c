660c18b56fcad9cfa805448fc344c204e82b825d
LTakamori <925951065@qq.com>
2020-11-16 18:14:30 +0800

    finish lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL) return 0;
+    node *tortoise, *hare;
+    tortoise = head;
+    hare = head;
+    while (1) {
+        for (int i = 0; i < 2; i++) {
+            if (hare->next == NULL) {
+                return 0;
+            } else {
+                hare = hare->next;
+            }
+        }
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
